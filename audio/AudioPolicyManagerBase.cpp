1ad0c9864b77aabc9b22417a292c488fe98e2e2d
b02247 <b02247@freescale.com>
2013-02-28 15:18:15 +0800

    ENGR00242652 user can't tune the volume on hdmidongle
    specify hdmidongle when tuning the volume
    
    Signed-off-by: b02247 <b02247@freescale.com>

--- audio/AudioPolicyManagerBase.cpp
+++ audio/AudioPolicyManagerBase.cpp
@@ -31,6 +31,8 @@
 // A device mask for all audio output devices that are considered "remote" when evaluating
 // active output devices in isStreamActiveRemotely()
 #define APM_AUDIO_OUT_DEVICE_REMOTE_ALL  AUDIO_DEVICE_OUT_REMOTE_SUBMIX
+#define PRODUCT_DEVICE_PROPERTY "ro.product.device"
+#define PRODUCT_DEVICE_HDMIDONGLE     "hdmidongle"
 
 #include <utils/Log.h>
 #include <hardware_legacy/AudioPolicyManagerBase.h>
@@ -38,6 +40,7 @@
 #include <hardware/audio.h>
 #include <math.h>
 #include <hardware_legacy/audio_policy_conf.h>
+#include <cutils/properties.h>
 
 namespace android_audio_legacy {
 
@@ -1336,6 +1339,10 @@ AudioPolicyManagerBase::AudioPolicyManagerBase(AudioPolicyClientInterface *clien
     mTotalEffectsCpuLoad(0), mTotalEffectsMemory(0),
     mA2dpSuspended(false), mHasA2dp(false), mHasUsb(false), mHasRemoteSubmix(false)
 {
+    char property[PROPERTY_VALUE_MAX];
+    property_get(PRODUCT_DEVICE_PROPERTY, property, "");
+    mDeviceIsHdmidongle = (strstr(property, PRODUCT_DEVICE_HDMIDONGLE) != NULL);
+
     mpClientInterface = clientInterface;
 
     for (int i = 0; i < AudioSystem::NUM_FORCE_USE; i++) {
@@ -2784,7 +2791,7 @@ float AudioPolicyManagerBase::computeVolume(int stream,
     // if volume is not 0 (not muted), force media volume to max on digital output
     if (stream == AudioSystem::MUSIC &&
         index != mStreams[stream].mIndexMin &&
-        (device == AUDIO_DEVICE_OUT_AUX_DIGITAL ||
+        (((device == AUDIO_DEVICE_OUT_AUX_DIGITAL) && (mDeviceIsHdmidongle == false)) ||
          device == AUDIO_DEVICE_OUT_DGTL_DOCK_HEADSET ||
          device == AUDIO_DEVICE_OUT_USB_ACCESSORY ||
          device == AUDIO_DEVICE_OUT_USB_DEVICE)) {
--- include/hardware_legacy/AudioPolicyManagerBase.h
+++ include/hardware_legacy/AudioPolicyManagerBase.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2013 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -551,6 +552,7 @@ protected:
                                               // (must be in mAttachedOutputDevices)
 
         Vector <HwModule *> mHwModules;
+        bool mDeviceIsHdmidongle;
 
 #ifdef AUDIO_POLICY_TEST
         Mutex   mLock;
