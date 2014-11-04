391e3339b425096d46829f5dc1f4177e1c1ae12d
Wang Haoran <b50027@freescale.com>
2014-11-04 13:22:25 +0800

    MA-5834 Fix wifi failure for sabreauto_6sx board
    One macro use not correct and it will make load
    dirvers failure. Correct it and the wifi driver  for
    sabreauto_6sx will be loaded successfully.
    
    Signed-off-by: Wang Haoran <b50027@freescale.com>

--- wifi/wifi_unite.c
+++ wifi/wifi_unite.c
@@ -1,6 +1,6 @@
 /*
  * Copyright 2008, The Android Open Source Project
- * Copyright (C) 2013 Freescale Semiconductor, Inc.
+ * Copyright (C) 2014 Freescale Semiconductor, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -196,7 +196,7 @@ int get_wifi_vendor_info(int* model)
         SYS_SDIO_BUS_MMC_PATH_DEVICE(0),
         SYS_SDIO_BUS_MMC_PATH_DEVICE(1),
         SYS_SDIO_BUS_MMC_PATH_DEVICE(2),
-        SYS_SDIO_BUS_MMC_PATH_DEVICE(4),
+        SYS_SDIO_BUS_MMC_PATH_DEVICE(3),
     };
     char linebuf[1024];
     long value;
