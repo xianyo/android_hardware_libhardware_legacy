1d134b7a4031ee35c6f2b1620e0027ba2697ac09
Gary Bisson <gary.bisson@boundarydevices.com>
2015-04-09 09:44:03 +0200

    wifi: support adding p2p0 when starting wpa_supplicant
    Add p2p0 using an NL80211 command when wpa_supplicant is started, and
    p2p_supported is enabled.
    
    [Port from TI omapzoom repo, see commit ccb4c24d]
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>
    Signed-off-by: Arik Nemtsov <arik@wizery.com>

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -60,4 +60,10 @@ else
   LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../external/wpa_supplicant_8/src/common
 endif
 
+ifdef USES_TI_MAC80211
+  LOCAL_STATIC_LIBRARIES := libnl_2
+  LOCAL_C_INCLUDES += external/libnl-headers
+  LOCAL_C_INCLUDES += hardware/ti/wlan/mac80211/wpa_supplicant_lib
+endif
+
 LOCAL_SHARED_LIBRARIES += libnetutils
