d807f1c1020f2a2e35a3db82b9765f937db73e7d
Haoran Wang <b50027@freescale.com>
2015-05-15 10:02:32 +0800

    MA-6450 Enable BCM4339 wifi
    Added BCM4339 support on wifi HAL.
    Use BCM4339 in sabresd_7d by force.
    
    Signed-off-by: Haoran Wang <b50027@freescale.com>

--- wifi/Android.mk
+++ wifi/Android.mk
@@ -51,6 +51,9 @@ endif
 
 ifeq ($(BOARD_WLAN_DEVICE),UNITE)
   LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../external/wpa_supplicant_ath/wpa_supplicant/src/common
+ifeq ($(TARGET_PRODUCT),sabresd_7d)
+  LOCAL_CFLAGS += -DSABRESD_7D
+endif
   LOCAL_SRC_FILES += wifi/wifi_unite.c
 else ifeq ($(BOARD_WLAN_VENDOR), INTEL)
   LOCAL_SRC_FILES += wifi/wifi_intel.c
